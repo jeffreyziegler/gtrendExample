b28dba78c3c255fbd8cff8b53f16049c4cec4a71
zieglerjef <zieglerjef@gmail.com>
2016-06-07 19:25:40 -0500

    Update app file

--- gtrendsApp.R
+++ gtrendsApp.R
@@ -7,19 +7,28 @@ library(gtrendsR)
 library(R.utils)
 library(RColorBrewer)
 
+# set default logged in status to FALSE
 Logged = FALSE
 
+# create sign-in/initial page
 ui1 <- function(){
   tagList(
+    # label div "login"
     div(id = "login",
+        # set header of page
         h3("Google Log-In"),
+        # create log-in panel w/ username and password
         wellPanel(textInput("userName", "Username"),
-                  passwordInput("passwd", "Password"),
-                  br(),actionButton("Login", "Log in")),
+            passwordInput("passwd", "Password"),
+            # create log-in button
+            br(),actionButton("Login", "Log in")),
+                  # add comment below button
                   p("Please remain patient while Google connects and loads search trend data.")),
+        # stylize page
         tags$style(type="text/css", "#login {font-size:10px;   text-align: left;position:absolute;top: 40%;left: 50%;margin-top: -100px;margin-left: -150px;}")
   )}
 
+# create secondary page: scatter plot and map of search trend
 ui2 <- function(){
   tagList(
   # start page
@@ -37,6 +46,7 @@ ui2 <- function(){
                     # trend output
                     ggvisOutput("plot1")
                   ),
+                  # title of page
                   mainPanel("Relative frequency of ISIS searches by state",
                             fluidRow(
                               # map output
@@ -46,41 +56,40 @@ ui2 <- function(){
   ) 
 )}
 
-# begin shiny app
+# call page with htmlOutput
 ui = (htmlOutput("page"))
 
-# shiny app
+# begin server function of shiny app
 server = function(input, output, session){
-    
+    # logged in status determined by log-in from ui1()
     USER <- reactiveValues(Logged = Logged)
-    
+    # since the default is set to FALSE
     observe({ 
       if (USER$Logged == FALSE) {
+        # determine if valid value is present to log-in
         if (!is.null(input$Login)) {
           if (input$Login > 0) {
             Username <- isolate(input$userName)
             Password <- isolate(input$passwd)
-            # login to google
+            ## attempt to log-in to google
+            # rename variables for gconnect()
             gconnect(usr = Username, psw = Password, verbose = FALSE)
-            if (length(Username) > 0 & length(Password) > 0) {
-              if (!is.null(gconnect(usr=Username, psw=Password))) {
+            # if log-in is successful change logged status to TRUE
+            if (!is.null(gconnect(usr=Username, psw=Password))) {
                 USER$Logged <- TRUE
-              } 
             }
           } 
         }
-      }    
-    })
-    observe({
-      if (USER$Logged == FALSE) {
-        
+        # once user logs in execute ui1 function
         output$page <- renderUI({
           div(class="outer",do.call(bootstrapPage,c("",ui1())))
         })
-      }
+    }})
+    # once log-in is successful
+    observe({
       if (USER$Logged == TRUE) 
       {
-        # Filter the isis data, returning a data frame with data to construct map
+        # filter isis data, returning a data frame with data to construct map
         map <- reactive({
           # create variable to change resolution of trending data
           # load data create input variables 
@@ -146,16 +155,20 @@ server = function(input, output, session){
                  mapHover$United.States, "<br>")
         }
         
-        # render map output
+        # render scatterplot output
         visualLine <- reactive({
           
           # open plot
           scatter %>%
             ggvis(~start, y = ~ISIS.US) %>%
+            # point outline
             layer_points(stroke := "white") %>%
+            # add smoother
             layer_model_predictions(model = "loess", se = TRUE, fill := "YlOrRd") %>%
+            # label axes
             add_axis("x", title = "Time") %>%
             add_axis("y", title = "Relative frequency of ISIS search") %>%
+            # set plot dimensions
             set_options(width=500, height=350, keep_aspect=TRUE)
         })
         
@@ -166,23 +179,29 @@ server = function(input, output, session){
           map %>%
             group_by(group) %>%
             ggvis(~long, ~lat) %>%
+            # determine fill colors
             layer_paths(fill:=~fillColor, strokeOpacity := 0.5, strokeWidth := 0.1) %>%
+            # add hover tool
             add_tooltip(isis_tooltip, "hover") %>%
+            # hide axes
             hide_axis("x") %>%
             hide_axis("y") %>%
+            # set plot dimensions
             set_options(width=850, height=550, keep_aspect=TRUE)
         })
         
         # label visualization output
         visualLine %>% bind_shiny("plot1")
         visualMap %>% bind_shiny("plot2")
-        
+        # execute ui2 function
         output$page <- renderUI({
           div(class="outer",do.call(navbarPage,c(inverse=T, title = "Web Search interest: 'ISIS'",ui2())))
         })
+        # print ui functions jointly
         print(ui)
       }
     })
 }
 
+# run app
 runApp(list(ui = ui, server = server))
